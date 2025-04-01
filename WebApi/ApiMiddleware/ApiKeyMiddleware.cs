6d3cdfa3b061cd1a305680bdb47c1bd3ddb9e176
Calle <carl.jonsen@yh.nackademin.se>
2025-04-01 15:39:08 +0200

    Added an ApiKey to swagger. Bit more secure now!

---
+++ WebApi/ApiMiddleware/ApiKeyMiddleware.cs
@@ -0,0 +1,36 @@
+﻿namespace WebApi.ApiMiddleware
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string APIKEY_HEADER = "x-api-key";
+        private readonly IConfiguration _configuration;
+
+        public ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(APIKEY_HEADER, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("API-nyckel krävs.");
+                return;
+            }
+
+            var apiKey = _configuration.GetSection("ApiSettings:SecretKey").Value;
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Ogiltig API-nyckel.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
